4261814ae396e46a11fb64c90c69ee108becaec3
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-08-25 19:45:56 -0300

    Aula 04 Operadores Matemáticos

---
+++ Aula 04 Operadores Matematicos/Aula04.js
@@ -0,0 +1,17 @@
+let num1=15;
+let num2=2;
+
+
+
+//res=num1+num2;
+//res=num1-num2;
+//res=num1*num2;
+//res=num1/num2;
+//res=num1%num2; Restante da Divisão
+
+num1++;
+num1--;
+num1+=1; // num1= num1+ 1;
+
+
+console.log(num1);
