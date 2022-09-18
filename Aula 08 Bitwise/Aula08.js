bc4eb293e6e480ad4d14c5778534b6bb2e96f141
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-09-18 15:46:20 -0300

    Aula 08 Bitwise
    Deslocação dos Bits

---
+++ Aula 08 Bitwise/Aula08.js
@@ -0,0 +1,13 @@
+let n1,n2,res
+
+n1=10
+n2=11
+
+//res=n1&n2  Faz uma comparação AND no Binario do Numero 10 e 11 retornando o Numero do Resultado
+//res=n1|n2  Faz uma comparação OR no Binario do Numero 10 e 11 retornando o Numero do Resultado
+//res=n1^n2  Faz uma comparação OU EXCLUSIVO no Binario do Numero 10 e 11 retornando o Numero do Resultado
+
+//res= n1<<1 Move o Binario do Numero n1 para Esquerda, Adicionando um 0 ao final, Dobrando o seu valor EX: 00001001 -> 00010100
+
+//res= n1>>1 Move o Binario do Numero n1 para Direita, Removendo o ultimo numero, Reduzindo o seu valor para metade EX: 10(DEC)  00001001 -> 00000100
+console.log(res)
