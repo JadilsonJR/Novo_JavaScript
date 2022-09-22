63c258a7f75a804a86b502fd80a3927a3f59b1f1
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-09-21 22:02:32 -0300

    Aula 10 Operador Ternário

---
+++ Aula 10 Operador Ternario/Aula 10.js
@@ -0,0 +1,23 @@
+
+//OPERAÇÂO TERNARIA
+
+// Teste Logico ? Se Verdadeiro : Se Falso
+// 0 Falso 
+// 1 Verdadeiro
+
+let num=17;
+
+let res=(!(num % 2) ? "PAR" : "IMPAR" ) 
+
+console.log(res)
+
+
+
+
+//IF NORMAL 
+// if(res==0){
+//     console.log("PAR")
+// }else{
+//     console.log("IMPAR")
+// }
+
