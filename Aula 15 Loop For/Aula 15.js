5aa838b55f9d2ad134c1a34c174c0313d2bbe655
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-04 09:20:19 -0300

    Aula 15 Comando FOR

---
+++ Aula 15 Loop For/Aula 15.js
@@ -0,0 +1,19 @@
+/*
+ Loops - Definidos : For 
+ Loops - Indefinidos : While , Do while
+
+ FOR ( INICIALIZAÇÂO ; CONDIÇÂO , CONTROLE ){
+ }
+*/
+
+
+for (let i=0; i<100;i++){
+    
+    if(i%2 ==0 ){
+        console.log("PAR: "+i);
+    }else{
+        console.log("IMPAR : "+i)
+    }
+
+    
+}
