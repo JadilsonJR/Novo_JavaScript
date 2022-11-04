d540aa5d0076637fbb5861fd52aad02bad3a7912
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-04 12:20:17 -0300

    Aula 16  Loops For in e For Of

---
+++ Aula 16 For in, For OF/Aula 16.js
@@ -0,0 +1,32 @@
+
+let num=[10,20,30,40,50]
+const objs=document.getElementsByTagName("div")
+
+
+//For normal Percorrendo um Vetor pelo Indice 
+// for (let i=0; i<num.length;i++){
+//     console.log(num[i])
+// }
+
+
+// //For in retornar o indice do elemento 
+// for( i in num){
+//     console.log(num[i])
+// }
+
+// //For of intera diretamente o valor do elemento 
+// for( i in num){
+//     console.log(i)
+// }
+
+
+
+//For in retornar a posição dos elemento 
+for( i in objs){
+    console.log(i)
+}
+
+//For of retorna o valor dos elementos 
+for( i in objs){
+    console.log(i)
+}
