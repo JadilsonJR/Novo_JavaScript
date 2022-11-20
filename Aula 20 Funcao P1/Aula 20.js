338acf97c445a8f65fd618f5372330f96ac78bcf
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-20 19:27:44 -0300

    Aula 20 Funções Criando e Chamando Funções

---
+++ Aula 20 Funcao P1/Aula 20.js
@@ -0,0 +1,12 @@
+//Função é um bloco de Comandos que pode ser executada quando for chamada
+
+
+function soma2_10(){ //Declaração da Função 
+    let n1=2
+    let n2=10
+    let soma = n1+n2
+    console.log("Soma: "+soma)
+}
+
+soma2_10()  //Chamada da Função
+
