36d8aab00eb43cf81181d2fbef8d4d1129a639fb
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-20 18:41:19 -0300

    Loop While

---
+++ Aula 17 Loop While/Aula 17.js
@@ -0,0 +1,20 @@
+//Loop While executa o loop enquanto a condição for verdadeira 
+
+/*
+let n=0
+while(n<10){
+    console.log(n)
+    n++
+}
+
+*/
+
+let n=5
+let fatorial=1
+
+while(n>=1){
+    fatorial*=n
+    n-=1
+}
+
+console.log(fatorial)
