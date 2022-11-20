5ee17656804ae491f25750dcd3ffe833348391fd
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-20 19:57:13 -0300

    Funções com Retornos

---
+++ Aula 21 Funcao P2/Aula 22.js
@@ -0,0 +1,16 @@
+
+//Funçãom com retorno de chamada
+function par_impar(){
+    let n1=10
+    let n2=2
+    let res=n1*n2
+    if(res%2==0){
+        return "PAR"
+    }else{
+        return "IMPAR"
+    }
+}
+
+let valor= par_impar()
+
+console.log(valor)
