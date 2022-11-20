b82a6fb34f9c371344d821c2dec138ed6e8c96cd
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2022-11-20 18:52:18 -0300

    Diferencia entre o Loop While e Do while

---
+++ Aula 18 Diferencia de While e Do While/Aula 18.js
@@ -0,0 +1,15 @@
+// While primeira executa o teste logico e depois executa
+// Do While executa primeiro e depois testa a condição
+
+let n = 10
+
+while (n < 10) {
+    console.log("CFB Cursos")
+    n++
+}
+
+do {
+    console.log("CFB Cursos")
+    n++
+} while (n < 10)
+console.log("Fim do Programa")
